f85e7016a393571343e35debc5ef0bc49a070278
NFintak <natefintak@gmail.com>
2025-08-17 17:54:51 -0400

    checkpoint

--- src/main/java/io/zipcoder/persistenceapp/Department.java
+++ src/main/java/io/zipcoder/persistenceapp/Department.java
@@ -1,4 +1,14 @@
 package io.zipcoder.persistenceapp;
 
+import javax.persistence.*;
+import io.zipcoder.persistenceapp.Employee;
+
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long deptId;
+    private String deptName;
+    private Employee deptManager;
+
 }
