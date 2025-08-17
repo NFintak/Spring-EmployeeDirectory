02a5ec9e449b46b47b872a481899a01a60a2779c
NFintak <natefintak@gmail.com>
2025-08-17 18:11:47 -0400

    checkpoint

--- src/main/java/io/zipcoder/persistenceapp/Employee.java
+++ src/main/java/io/zipcoder/persistenceapp/Employee.java
@@ -19,4 +19,42 @@ public class Employee {
     private String manager;
     private int deptNum;
 
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPhoneNumber(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNum;
+    }
+
+
+
 }
