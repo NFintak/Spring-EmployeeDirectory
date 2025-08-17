b139018b06125bf674bf73830919c65715c68fa7
NFintak <natefintak@gmail.com>
2025-08-17 19:28:20 -0400

    adds imports and notes to controller classes

---
+++ src/main/java/io/zipcoder/persistenceapp/Controllers/DeptController.java
@@ -0,0 +1,19 @@
+package io.zipcoder.persistenceapp.Controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+//import io.zipcoder.persistenceapp.Models.Department;
+//import io.zipcoder.persistenceapp.Models.Employee;
+
+//all endpoints should start with '/API'
+
+public class DeptController {
+}
