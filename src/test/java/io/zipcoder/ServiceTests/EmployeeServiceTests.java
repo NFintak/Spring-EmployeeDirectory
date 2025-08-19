2d05f0a2a0cb87c060423e57eb5b22291f862179
NFintak <natefintak@gmail.com>
2025-08-19 13:29:31 -0400

    troubleshooting EmployeeServiceTests

--- src/test/java/io/zipcoder/ServiceTests/EmployeeServiceTests.java
+++ src/test/java/io/zipcoder/ServiceTests/EmployeeServiceTests.java
@@ -3,47 +3,48 @@ package io.zipcoder.ServiceTests;
 import io.zipcoder.persistenceapp.Models.Employee;
 import io.zipcoder.persistenceapp.Services.EmployeeService;
 import io.zipcoder.persistenceapp.Controllers.EmployeeController;
-import io.zipcoder.persistenceapp.Repos.EmployeeRepo;
 import io.zipcoder.persistenceapp.PersistenceStarterApplication;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.Before;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = PersistenceStarterApplication.class)
+@SpringBootTest(classes = PersistenceStarterApplication.class)
 public class EmployeeServiceTests {
 
     @MockBean
     private EmployeeService service;
 
-    @InjectMocks
     private EmployeeController controller;
-
-//    @MockBean
-//    private EmployeeRepo repo;
 
     @Before
     public void setUp() {
-        //this.service = new EmployeeService(repo);
         this.controller = new EmployeeController(service);
     }
 
     @Test
     public void testCreateEmployee() {
+        //given
         HttpStatus expected = HttpStatus.CREATED;
         Employee expectedEmployee = new Employee("Jane", "Doe");
         BDDMockito.given(service.create(expectedEmployee)).willReturn(expectedEmployee);
+        //when
         ResponseEntity<Employee> response = controller.createEmployee(expectedEmployee);
         HttpStatus actual = response.getStatusCode();
         Employee actualEmployee = response.getBody();
+        //then
         assertEquals(expected, actual);
         assertEquals(expectedEmployee, actualEmployee);
     }
