d5a0f09e8ab5eb65550012b4f66b662e6259dad9
NFintak <natefintak@gmail.com>
2025-09-01 15:34:26 -0400

    checkpoint

--- src/main/java/io/zipcoder/persistenceapp/models/Employee.java
+++ src/main/java/io/zipcoder/persistenceapp/models/Employee.java
@@ -48,4 +48,72 @@ public class Employee {
         this.deptNum = deptNum;
     }
 
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setDeptNum(int deptNum) {
+        this.deptNum = deptNum;
+    }
+
+    public int getDeptNum() {
+        return deptNum;
+    }
+
 }
