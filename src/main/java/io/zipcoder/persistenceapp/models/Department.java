4d56225d1824a7b799d12988dc41b8a1557f2027
NFintak <natefintak@gmail.com>
2025-09-01 16:24:20 -0400

    start of DepartmentTests

--- src/main/java/io/zipcoder/persistenceapp/models/Department.java
+++ src/main/java/io/zipcoder/persistenceapp/models/Department.java
@@ -1,4 +1,44 @@
 package io.zipcoder.persistenceapp.models;
 
+import javax.persistence.*;
+import io.zipcoder.persistenceapp.models.Employee;
+
+@Entity
 public class Department {
+
+    private int deptId;
+
+    private String deptName;
+    private Employee deptManager;
+
+    public Department() {
+        this.deptName = "";
+        this.deptManager = null;
+    }
+
+    public Department(String deptName, Employee deptManager) {
+        this.deptName = deptName;
+        this.deptManager = deptManager;
+    }
+
+    public int getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptManager(Employee deptManager) {
+        this.deptManager = deptManager;
+    }
+
+    public Employee getDeptManager() {
+        return deptManager;
+    }
+
 }
--- src/test/java/io/zipcoder/models/DepartmentTests.java
+++ src/test/java/io/zipcoder/models/DepartmentTests.java
@@ -1,4 +1,9 @@
 package io.zipcoder.models;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import io.zipcoder.persistenceapp.models.Department;
+import io.zipcoder.persistenceapp.models.Employee;
+
 public class DepartmentTests {
 }
