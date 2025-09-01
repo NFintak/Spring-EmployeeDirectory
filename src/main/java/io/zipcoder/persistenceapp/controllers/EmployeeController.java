3520037e8b8e4bc2e0f6ef7ebe6104e43f480d12
NFintak <natefintak@gmail.com>
2025-09-01 17:02:04 -0400

    checkpoint

--- src/main/java/io/zipcoder/persistenceapp/controllers/EmployeeController.java
+++ src/main/java/io/zipcoder/persistenceapp/controllers/EmployeeController.java
@@ -1,4 +1,21 @@
 package io.zipcoder.persistenceapp.controllers;
 
+import io.zipcoder.persistenceapp.models.Employee;
+import io.zipcoder.persistenceapp.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
 }
--- src/main/java/io/zipcoder/persistenceapp/services/EmployeeService.java
+++ src/main/java/io/zipcoder/persistenceapp/services/EmployeeService.java
@@ -1,4 +1,20 @@
 package io.zipcoder.persistenceapp.services;
 
+import io.zipcoder.persistenceapp.models.Employee;
+import io.zipcoder.persistenceapp.repos.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepo repo;
+
+    public EmployeeService(EmployeeRepo repo) {
+        this.repo = repo;
+    }
+
 }
