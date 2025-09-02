35b8fadffd45b6495d7cbea837d57343be0ecca7
NFintak <natefintak@gmail.com>
2025-09-02 14:09:08 -0400

    troubleshooting tests

--- src/test/java/io/zipcoder/controllers/EmployeeControllerTest.java
+++ src/test/java/io/zipcoder/controllers/EmployeeControllerTest.java
@@ -1,4 +1,45 @@
 package io.zipcoder.controllers;
 
+import io.zipcoder.persistenceapp.models.Employee;
+import io.zipcoder.persistenceapp.repos.EmployeeRepo;
+import io.zipcoder.persistenceapp.controllers.EmployeeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import java.util.Optional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
 public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private EmployeeRepo repo;
+
+    @Test
+    public void testShowOne() throws Exception {
+        Long givenId = 1L;
+        BDDMockito
+                .given(repo.findOne(givenId))
+                .willReturn(new Employee("Jane", "Doe"));
+        String expectedContent = "{\"employeeId\":null,\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"title\":\"\"," +
+                "\"phoneNum\":\"\",\"email\":\"\",\"hireDate\":null,\"manager\":null,\"deptNum\":0}";
+        this.mvc.perform(MockMvcRequestBuilders
+                .get("/employees/" + givenId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedContent));
+    }
+
 }
