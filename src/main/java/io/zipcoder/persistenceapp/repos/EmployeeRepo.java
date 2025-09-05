10f62cb8f8ba795cab0919fe3d6eee76ef3c7a5e
NFintak <natefintak@gmail.com>
2025-09-05 11:44:55 -0400

    troubleshooting issues with employee controller test

--- src/main/java/io/zipcoder/persistenceapp/repos/EmployeeRepo.java
+++ src/main/java/io/zipcoder/persistenceapp/repos/EmployeeRepo.java
@@ -2,6 +2,8 @@ package io.zipcoder.persistenceapp.repos;
 
 import io.zipcoder.persistenceapp.models.Employee;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepo extends CrudRepository<Employee, Long> {
 }
--- src/test/java/io/zipcoder/controllers/EmployeeControllerTest.java
+++ src/test/java/io/zipcoder/controllers/EmployeeControllerTest.java
@@ -1,11 +1,15 @@
 package io.zipcoder.controllers;
 
+import io.zipcoder.persistenceapp.controllers.EmployeeController;
 import io.zipcoder.persistenceapp.models.Employee;
 import io.zipcoder.persistenceapp.repos.EmployeeRepo;
 import io.zipcoder.persistenceapp.PersistenceStarterApplication;
+import io.zipcoder.persistenceapp.services.EmployeeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest //(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = PersistenceStarterApplication.class)
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = PersistenceStarterApplication.class)
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class EmployeeControllerTest {
@@ -26,6 +30,12 @@ public class EmployeeControllerTest {
 
     @MockBean
     private EmployeeRepo repo;
+
+    @Mock
+    private EmployeeService employeeService;
+
+    @InjectMocks
+    private EmployeeController employeeController;
 
     @Test
     public void testShowOne() throws Exception {
